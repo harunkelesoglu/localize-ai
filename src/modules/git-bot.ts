a2ef18b3789134d7d51c080b03e79500e3a6f28d
harunkelesoglu <harunkelesoglu_@hotmail.com>
2023-06-23 01:04:22 +0300

    feat: added gitbot for ci

---
+++ src/modules/git-bot.ts
@@ -0,0 +1,59 @@
+import fs from 'fs';
+import { IConfig } from "../config";
+import { execSync } from 'child_process';
+import logger from '../utils/logger';
+import path from 'path';
+
+const mainModulePath = require.main?.filename;
+
+const projectDirectory = mainModulePath ? path.dirname(mainModulePath) : '';
+const packageJsonPath = path.join(projectDirectory, 'package.json');
+
+export class GitBot {
+    private config: IConfig;
+    constructor(config: IConfig){
+        this.config = config;
+    }
+
+    public hasTranslationChanges(): boolean {
+        const { localesDir, baseLanguage } = this.config;
+        const diffOutput = execSync(`git diff --name-only HEAD^ ${localesDir}/${baseLanguage}.json`).toString().trim();
+        return diffOutput.length > 0;
+    }
+
+    public createAndCheckoutBranch(translationBranch: string): void {
+        
+        logger.debug(`[GitBot] branch is creating...`, { translationBranch });
+        execSync(`
+        if git rev-parse --verify --quiet ${translationBranch}; then
+            git checkout ${translationBranch}
+         else
+            git checkout -b ${translationBranch}
+        fi`);
+    }
+
+    public stagedChanges(): void{
+        const { localesDir } = this.config;
+
+        logger.debug('[GitBot] changes staging...');
+        execSync(`git add ${localesDir}/*.json`);
+    }
+
+    public commitChanges(): void {
+
+        logger.debug('[GitBot] changes committing...');
+        execSync('git commit -m "chore: Translations added"');
+    }
+
+    public pushChanges(translationBranch: string): void {
+
+        logger.debug('[GitBot] changes pushing...', { translationBranch });
+        execSync(`git push origin ${translationBranch}`);
+    }
+
+    public createPullRequest(baseBranch: string, translationBranch: string) {
+
+        logger.debug('[GitBot] pull request creating...', { baseBranch, translationBranch });
+        execSync(`gh pr create --base ${baseBranch} --head ${translationBranch} --title "localize-ai: Translation Update" --body "Translations completed"`);
+    } 
+}   
