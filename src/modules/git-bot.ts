339e88b31010532d18aad2c10aabc0d482a0803f
harunkelesoglu <harunkelesoglu_@hotmail.com>
2023-06-23 19:23:27 +0300

    fix: fixed git diff command to avoid throwing exception

--- src/modules/git-bot.ts
+++ src/modules/git-bot.ts
@@ -17,8 +17,10 @@ export class GitBot {
 
     public hasTranslationChanges(): boolean {
         const { localesDir, baseLanguage } = this.config;
-        const diffOutput = execSync(`git diff --name-only HEAD^ ${localesDir}/${baseLanguage}.json`).toString().trim();
-        return diffOutput.length > 0;
+
+        logger.debug(`[GitBot] checking diff...`);
+        const diffOutput = execSync("git diff --name-only HEAD^").toString().trim();
+        return diffOutput.includes(`${localesDir}/${baseLanguage}.json`);
     }
 
     public createAndCheckoutBranch(translationBranch: string): void {
